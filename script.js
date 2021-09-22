acbbe18f27d0a56d81885e32f0e852effb4e3fd6
Ajay K <ajaycoolkarni@yahoo.com>
2021-09-23 03:57:08 +0930

    Javascript updated with while loop

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // User input variables: 
-var enter;
+var length;
 var confirmNumber;
 var confirmCharacter;
 var confirmUppercase;
@@ -25,76 +25,57 @@ field.addEventListener("click", function () {
 // Start function to generate password
 function generatePassword() {
   // Asks for user input
-  enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
-  // First if statement for user validation 
-  if (!enter) {
-    alert("This needs a value");
-  } else if (enter < 8 || enter > 128) {
-    // Validates user input
-    // Start user input prompts
-    enter = parseInt(prompt("You must choose between 8 and 128"));
-
-  } else {
-    // Continues once user input is validated
-    confirmNumber = confirm("Will this contain numbers?");
-    confirmCharacter = confirm("Will this contain special characters?");
-    confirmUppercase = confirm("Will this contain Uppercase letters?");
-    confirmLowercase = confirm("Will this contain Lowercase letters?");
-  };
+  var length = 0;
+  while (length < 8 || length > 128) {
+    length = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
+  }
+  // Continues once user input is validated
+  confirmNumber = confirm("Will this contain numbers?");
+  confirmCharacter = confirm("Will this contain special characters?");
+  confirmUppercase = confirm("Will this contain Uppercase letters?");
+  confirmLowercase = confirm("Will this contain Lowercase letters?");
 
   // Else if for 4 negative options
   if (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
-    choices = alert("You must choose a criteria!");
+    length = alert("You must choose a criteria!");
   }
   // First if statement that uses user input prompts to determine choices
   // Else if for 4 positive options
   else if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase) {
-
     choices = character.concat(number, alpha, alpha2);
   }
   // Else if for 3 positive options
   else if (confirmCharacter && confirmNumber && confirmUppercase) {
     choices = character.concat(number, alpha2);
-  }
-  else if (confirmCharacter && confirmNumber && confirmLowercase) {
+  } else if (confirmCharacter && confirmNumber && confirmLowercase) {
     choices = character.concat(number, alpha);
-  }
-  else if (confirmCharacter && confirmLowercase && confirmUppercase) {
+  } else if (confirmCharacter && confirmLowercase && confirmUppercase) {
     choices = character.concat(alpha, alpha2);
-  }
-  else if (confirmNumber && confirmLowercase && confirmUppercase) {
+  } else if (confirmNumber && confirmLowercase && confirmUppercase) {
     choices = number.concat(alpha, alpha2);
   }
-  // Else if for 2 positive options 
+  // Else if for 2 positive options
   else if (confirmCharacter && confirmNumber) {
     choices = character.concat(number);
-
   } else if (confirmCharacter && confirmLowercase) {
     choices = character.concat(alpha);
-
   } else if (confirmCharacter && confirmUppercase) {
     choices = character.concat(alpha2);
-  }
-  else if (confirmLowercase && confirmNumber) {
+  } else if (confirmLowercase && confirmNumber) {
     choices = alpha.concat(number);
-
   } else if (confirmLowercase && confirmUppercase) {
     choices = alpha.concat(alpha2);
-
   } else if (confirmNumber && confirmUppercase) {
     choices = number.concat(alpha2);
   }
   // Else if for 1 positive option
   else if (confirmCharacter) {
     choices = character;
-  }
-  else if (confirmNumber) {
+  } else if (confirmNumber) {
     choices = number;
-  }
-  else if (confirmLowercase) {
+  } else if (confirmLowercase) {
     choices = alpha;
-  }
-  else if (confirmUppercase) {
+  } else if (confirmUppercase) {
     choices = alpha2;
   }
 
@@ -102,8 +83,8 @@ function generatePassword() {
   var password = [];
 
   // Start random selection variables:
-  // Random selection for all variables: 
-  for (var i = 0; i < enter; i++) {
+  // Random selection for all variables:
+  for (var i = 0; i < length; i++) {
     var pickChoices = choices[Math.floor(Math.random() * choices.length)];
     password.push(pickChoices);
   }
